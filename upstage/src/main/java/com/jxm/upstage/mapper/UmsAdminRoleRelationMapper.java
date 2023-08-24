2a6e4b2c29527fb95bd71c85b87f5fe884acc93b
wangrui <wangrui@qq.com>
2023-08-24 20:02:44 +0800

    商城项目后台代码框架---9.完成商品模块的产品类型相关功能

---
+++ upstage/src/main/java/com/jxm/upstage/mapper/UmsAdminRoleRelationMapper.java
@@ -0,0 +1,18 @@
+package com.jxm.upstage.mapper;
+
+
+import com.jxm.upstage.model.UmsAdminRoleRelation;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UmsAdminRoleRelationMapper {
+
+    int deleteById(Long adminId);
+
+    List<Long> selectById(Long id);
+
+    int deleteByAdminIds(@Param("ids") List<Long> ids);
+
+    List<UmsAdminRoleRelation> selectRoleListByAdminId(@Param("adminIds") List<Long> adminIds);
+}
